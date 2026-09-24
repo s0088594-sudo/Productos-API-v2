74e0473323c25877b6161534b7d60d8f7d604f97
s0088594-sudo <s0088594@gmail.com>
2026-09-24 00:51:10 -0500

    Initialize server with dotenv configuration

---
+++ Productos-API-v2-con-Middlewares-main/src/server.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import app from "./app";
+const PORT = Number(process.env.PORT) || 3000;
+app.listen(PORT, () => {
+ console.log(
+ `Servidor ejecutándose en http://localhost:${PORT}`
+ );
+});
