76d45019e23ca0b25aebcd7189ce47b00f541085
s0088594-sudo <s0088594@gmail.com>
2026-09-24 00:37:58 -0500

    Add admin middleware for role validation

---
+++ Productos-API-v2-con-Middlewares-main/src/middlewares/admin.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const adminMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+ const role = req.header("x-role");
+ if (!role) {
+ res.status(401).json({
+ message: "Debe enviar el rol del usuario"
+ });
+ return;
+ }
+ if (role !== "admin") {
+ res.status(403).json({
+ message: "Se requiere rol admin"
+ });
+ return;
+ }
+ next();
+}
