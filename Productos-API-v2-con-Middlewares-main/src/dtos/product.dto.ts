5da8cbad31bcd2cb6663b7e8328b6b512ebb74c9
s0088594-sudo <s0088594@gmail.com>
2026-09-24 00:35:23 -0500

    Add CreateProductDto and UpdateProductDto interfaces

---
+++ Productos-API-v2-con-Middlewares-main/src/dtos/product.dto.ts
@@ -0,0 +1,14 @@
+export interface CreateProductDto {
+ name: string;
+ price: number;
+ category: string;
+ stock: number;
+ active: boolean;
+}
+export interface UpdateProductDto {
+ name: string;
+ price: number;
+ category: string;
+ stock: number;
+ active: boolean;
+}
